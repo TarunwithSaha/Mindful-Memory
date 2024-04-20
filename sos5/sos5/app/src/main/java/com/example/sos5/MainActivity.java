0ac2c0b9432e6ff9ab51f7aaf0481889d743f469
Chitransh1011 <chitranshprasanna6666@gmail.com>
2024-04-21 00:47:22 +0530

    New Files Added

---
+++ sos5/sos5/app/src/main/java/com/example/sos5/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sos5;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void BtnSetEmergency_onClick(View view) {
+        String number = "123456789";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            return;
+        }
+
+        startActivity(intent);
+    }
+}
